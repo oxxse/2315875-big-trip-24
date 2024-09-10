d16e611a94dff92cc463dc7da5e48b1a150c4a42
Ksenia Lavrukhina <burkamera@gmail.com>
2024-09-10 22:02:24 +0300

    Deletes comment

--- src/utils.js
+++ src/utils.js
@@ -1,26 +1,3 @@
-// import dayjs from 'dayjs';
-
-// const DATE_FORMAT = 'DD/MM/YY HH:mm';
-
-// function getRandomNumber(min = 1, max = 100) {
-//   return Math.floor(min + Math.random() * (max + 1 - min));
-// }
-
-// function getRandomArrayElement(items) {
-//   return items[getRandomNumber(0, items.length - 1)];
-// }
-
-// function formatDate(date) {
-//   return date ? dayjs(date).format(DATE_FORMAT) : '';
-// }
-
-// function calculateDuration(startDate, endDate) {
-//   return dayjs(endDate).diff(startDate, 'd');
-// }
-
-// export {getRandomArrayElement, getRandomNumber, formatDate, calculateDuration};
-
-
 import dayjs from 'dayjs';
 
 const DATE_INPUT_FORMAT = 'DD/MM/YY HH:mm';
