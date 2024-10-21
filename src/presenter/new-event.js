a0cb97c1d9b77e0410c55dcef88bcbd036e4ef60
Ksenia Lavrukhina <burkamera@gmail.com>
2024-10-21 23:03:26 +0300

    Fixies form disable state

--- src/presenter/new-event.js
+++ src/presenter/new-event.js
@@ -21,7 +21,7 @@ export default class NewEvent {
   }
 
   setSaving() {
-    this.#formComponent.updateElement({ isSaving: true });
+    this.#formComponent.updateElement({ isSaving: true, isDisabled: true });
   }
 
   setAborting() {
