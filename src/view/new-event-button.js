567c4e2ec49232fc29877a5e467e660bb85fac1a
Лаврухина Ксения Николаевна <lavrukhinakn@iek.ru>
2025-01-09 16:02:53 +0300

    test copy button

--- src/view/new-event-button.js
+++ src/view/new-event-button.js
@@ -33,10 +33,13 @@ export default class NewEventButton extends AbstractView {
         });
         navigator.clipboard.write([clipboardItem]);
       } else {
-        const status = 'fkrewfdch,gb';
-        setTimeout(() => {
-          navigator.clipboard.writeText(status);
+        const clipboardItem = new ClipboardItem({
+          'text/plain': new Promise((resolve) => {
+            const status = 'fffffffffff';
+            resolve(new Blob([status]));
+          }),
         });
+        navigator.clipboard.write([clipboardItem]);
       }
     } catch (error) {
       // console.log('fdvbfdvbdf');
