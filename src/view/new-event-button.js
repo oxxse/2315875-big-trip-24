9126a57d95e35d69d8278e726611be216c407adb
Лаврухина Ксения Николаевна <lavrukhinakn@iek.ru>
2025-01-09 15:56:31 +0300

    test copy button

--- src/view/new-event-button.js
+++ src/view/new-event-button.js
@@ -1,17 +1,17 @@
-import AbstractView from '../framework/view/abstract-view';
+import AbstractView from "../framework/view/abstract-view";
 
 function createNewEventButton() {
-  return ('<button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>');
+  return '<button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>';
 }
 
 export default class NewEventButton extends AbstractView {
   #handleButtonClick = null;
 
-  constructor({onButtonClick}) {
+  constructor({ onButtonClick }) {
     super();
     this.#handleButtonClick = onButtonClick;
 
-    this.element.addEventListener('click', this.#buttonClickHandler);
+    this.element.addEventListener("click", this.#buttonClickHandler);
   }
 
   get template() {
@@ -20,6 +20,28 @@ export default class NewEventButton extends AbstractView {
 
   #buttonClickHandler = (evt) => {
     evt.preventDefault();
+    const userAgent = navigator.userAgent;
+    const isSafari = !!userAgent.match(/Version\/[\d.]+.*Safari/);
+    const isIphone = /iP(od|hone)/i.test(userAgent) && isSafari;
+    try {
+      if (isSafari || isIphone) {
+        const clipboardItem = new ClipboardItem({
+          'text/plain': new Promise((resolve) => {
+            const status = 'fkh,gb';
+            resolve(new Blob([status]));
+          }),
+        });
+        navigator.clipboard.write([clipboardItem]);
+      } else {
+        const status = 'fkrewfdch,gb';
+        setTimeout(() => {
+          navigator.clipboard.writeText(status);
+        });
+      }
+    } catch (error) {
+      // console.log('fdvbfdvbdf');
+    }
+    //dispatch(toggleFavoriteStatus({ offerId: offerId, status: status }));
     this.#handleButtonClick();
   };
 }
